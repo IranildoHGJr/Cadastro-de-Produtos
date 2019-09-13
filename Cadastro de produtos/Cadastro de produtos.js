acb39539f389af66b87231af096720eb179ab8b8
IranildoHGJr <49312528+IranildoHGJr@users.noreply.github.com>
2019-09-13 16:10:55 -0300

    teste

--- Cadastro de produtos/Cadastro de produtos.js
+++ Cadastro de produtos/Cadastro de produtos.js
@@ -7,11 +7,12 @@ function cal_media(n1,n2)
 function comfirmar(form)
 {
 
-    if (form.codigoProduto.value == 0||form.inputQuantidade.value == 0||form.inputDescricao.value == 0)
+    if (form.codigoProduto.value == 0||form.inputQuantidade.value == 0||form.inputDescricao.value == 0||form.nomedoproduto.value == 0)
 
     alert("Dados Invalidos");
     else
     alert("Produto registrado");
+    form.nomedoproduto.value = " ";
     form.codigoProduto.value = " ";
     form.inputQuantidade.value = "1";
     form.inputDescricao.value = " ";
@@ -19,6 +20,7 @@ function comfirmar(form)
 
 function limpar(form)
 {
+    form.nomedoproduto.value = " ";
     form.codigoProduto.value = " ";
     form.inputQuantidade.value = "1";
     form.inputDescricao.value = " ";
