c73f0bec54cadf2fa4554e6238fbd71c7d7fca3f
IranildoHGJr <49312528+IranildoHGJr@users.noreply.github.com>
2019-10-01 10:50:30 -0300

    1

--- Cadastro de produtos/Cadastro de produtos.js
+++ Cadastro de produtos/Cadastro de produtos.js
@@ -5,8 +5,9 @@ function processar(idTabela)
 	var newRow = d.createElement('tr');
 	newRow.insertCell(0).innerHTML = d.getElementsByName('codpro')[0].value;
     newRow.insertCell(1).innerHTML = d.getElementsByName('nompro')[0].value;
-    newRow.insertCell(0).innerHTML = d.getElementsByName('valprouni')[0].value;
-    newRow.insertCell(1).innerHTML = d.getElementsByName('despro')[0].value;
+    newRow.insertCell(2).innerHTML = d.getElementsByName('inpQua')[0].value;
+    newRow.insertCell(3).innerHTML = d.getElementsByName('valprouni')[0].value;
+    newRow.insertCell(4).innerHTML = d.getElementsByName('despro')[0].value;
     d.getElementById(idTabela).appendChild(newRow);
     return false;
 }
